d11be9697daaf6d2f1da0c42fdff741db26a03b3
Lakshyasunildubey <lakshyadubey3716@gmail.com>
2024-06-10 18:40:16 -0400

    completed step 2 and step3

--- lab2_script_template.py
+++ lab2_script_template.py
@@ -1,12 +1,23 @@
 def main():
+     # TODO: Step 2 - Create a complex data structure
+    student_profile = {
+        "full_name": "Lakshya Sunil Dubey",
+        "student_id": 10331244,
+        "pizza_toppings": ["PICKLED GINGER", "MINCED MUTTON", "PANEER"],
+        "movies": [
+            {"title": "Baahubali:The Beginning",
+            "genre": "Epic Fantasy"},
+            {"title": "Rangasthalam",
+            "genre": "Drama"}
+        ]}
 
-    # TODO: Step 2 - Create a complex data structure
-    about_me = {}
+   
 
     # TODO: Step 3 - Add another movie to the data structure
-    
+    student_profile["movies"].append({"title": "KGF: chapter 2","genre": "Action"})
+
 # TODO: Step 4 - Function that prints student name and ID	
-def print_student_name_and_id(about_me):
+def print_student_name_and_id(data):
     return
     
 # TODO: Step 5 - Function that adds pizza toppings to data structure
